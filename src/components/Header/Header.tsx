39adca6c81c9c69389e3b6d550aa9f17abdcf813
Ali YILMAZ <831590@gmail.com>
2025-07-12 18:15:14 +0300

    mobile responsive commiti

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,24 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
 import logo from '../../assets/logo.png';
 import { Link } from 'react-router-dom';
 
 const Header: React.FC = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
     <header className="header">
-      <div className="header__logo">
-        <Link to="/">
-          <img src={logo} alt="KARAÇAM & ŞİR" style={{ height: 44 }} />
-        </Link>
+      <div className="header__container">
+        <div className="header__logo">
+          <Link to="/" onClick={closeMenu}>
+            <img src={logo} alt="KARAÇAM & ŞİR" style={{ height: 44 }} />
+          </Link>
+        </div>
+        <button 
+          className="header__menu-toggle"
+          onClick={toggleMenu}
+          aria-label="Toggle menu"
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+        <nav className={`header__nav ${isMenuOpen ? 'header__nav--open' : ''}`}>
+          <Link to="/who-we-are" onClick={closeMenu}>Who We Are</Link>
+          <Link to="/what-we-are-doing" onClick={closeMenu}>What We Are Doing</Link>
+          <Link to="/how-we-are-doing" onClick={closeMenu}>How We Are Doing</Link>
+          <Link to="/blog" onClick={closeMenu}>Blog</Link>
+          <Link to="/careers" onClick={closeMenu}>Careers</Link>
+          <Link to="/contact-us" onClick={closeMenu}>Contact Us</Link>
+        </nav>
       </div>
-      <nav className="header__nav">
-        <Link to="/who-we-are">Who We Are</Link>
-        <Link to="/what-we-are-doing">What We Are Doing</Link>
-        <Link to="/how-we-are-doing">How We Are Doing</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/careers">Careers</Link>
-        <Link to="/contact-us">Contact Us</Link>
-      </nav>
     </header>
   );
 };
